bf46b42b9b28f81aab618b8e1e5c063f9a04c66b
vimalapolagani <98327616+vimalapolagani@users.noreply.github.com>
2023-01-28 11:28:26 +0530

    Add files via upload

---
+++ hack-wfh.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install autogui
+
+
+# In[ ]:
+
+
+import pyautogui
+import time
+pyautogui.FAILSAFE=False
+while True:
+    time.sleep(15)
+    for i in range(0,100):
+        pyautogui.moveTo(0,i*5)
+    for i in range(0,3):
+        pyautogui.press('shift')
+
+
+# In[ ]:
+
+
+
+
